6d2abd9fc6db229a1f545a6568935e01514f8226
yesol123 <yesolg90@gmail.com>
2023-10-31 09:58:41 +0900

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,60 @@
+
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <form @submit.prevent="insert">
+      <p><input type="text" name="name" v-model="name" /></p>
+      <p><input type="submit" value="저장" /></p>
+    </form>
   </div>
+
+  <ul>
+    <li v-for="item in data" :key="item.date">
+      {{ item.name }} / {{ item.count }}
+      <button @click="update(item.date, item.count)">좋아용</button>
+      <button @click="del(item.date)">삭제</button>
+    </li>
+  </ul>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+
+  //useEffect와 같은 놈
+  mounted() {
+    this.select();
+  },
+  methods: {
+    select() {
+      axios.get("http://localhost:3030/api")
+        .then(res => {
+          this.data = res.data;
+        })
+    },
+    update(date, count) {
+      axios.put(`http://localhost:3030/api/update?date=${date}`, { count: count + 1 })
+        .then(res => {
+          this.data = res.data;
+        })
+    },
+
+    insert() {
+      const data = { name: this.name, date: Date.now(), count: 0 }
+      axios.post("http://localhost:3030/api/insert", data)
+        .then(res => {
+          this.data = res.data;
+        })
+    },
+    del(date) {
+      axios.delete(`http://localhost:3030/api/delete?date=${date}`)
+        .then(res => {
+          this.data = res.data;
+        })
+    }
+  },
+  data() {
+    return { name: '', data: [] }
+  },
   name: 'HelloWorld',
   props: {
     msg: String
@@ -39,20 +62,4 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
